a1289836bece8108c035f83a676156754dc8c7a2
Daniel-SG <59225672+Daniel-SG@users.noreply.github.com>
2023-09-21 23:50:04 +0200

    Create samples.py

---
+++ samples.py
@@ -0,0 +1,40 @@
+import random
+import re
+from urllib.request import urlopen
+
+import requests
+from bs4 import BeautifulSoup
+from lxml import html
+
+import utils
+
+
+header = utils.get_header()
+
+url = "https://www.amazon.es/AmazonBasics-Perchas-terciopelo-trajes-Paquete/dp/B00FXNAAW2"
+
+# METODO POR ID
+# web_content = requests.get(url, headers=header)
+# parser = html.fromstring(web_content.text)
+# print(web_content.text)
+# desc =''
+# https://lxml.de/lxmlhtml.html
+# description = parser.get_element_by_id("feature-bullets")
+# description2 = parser.get_element_by_class("feature-bullets")
+# for texto in descripcion.text_content().split('     '):
+#    desc += texto
+
+# print(desc)
+
+# METODO POR XPARSER
+# web_content = requests.get(url, headers=header)
+# parser = html.fromstring(web_content.text)
+# pattern_title = f'//*[@id="productTitle"]/text()'
+# pattern_title2 = f'//div[contain(@class,'class_name')]//strong/text()'
+# title = str(parser.xpath(pattern)[0])
+
+# METODO BEAUTILSOUP
+# web_content = requests.get(url, headers=header)
+# soup = BeautifulSoup(web_content.text)
+# contenedor_preguntas = soup.find(id="questions")
+# lista_preguntas = contenedor_preguntas.find('div', class_="question_summary")
