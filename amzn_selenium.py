a77b55dbcae935c42d69c9134f8c4d8f8b91f56c
Daniel-SG <daniel.serrano.gine@gmail.com>
2023-10-08 23:35:01 +0200

    updated

--- amzn_selenium.py
+++ amzn_selenium.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from utils_amzn_selenium import get_title, get_currentPrice, get_descuento, get_precio_anterior, get_descripcion, \
+    get_foto_url
 
-def product_set(browser):
+
+def product_set(driver):
     products = []
-    products_list = browser.find_elements(By.CLASS_NAME, "a-list-item")
+    products_list = driver.find_elements(By.CLASS_NAME, "a-list-item")
 
     for product in products_list:
         detalles = product.text.split('\n')
@@ -18,41 +21,50 @@ def product_set(browser):
         products.append(l)
         # print(descuento, precio_actual, precio_anterior)
 
-    browser.quit()
+    driver.quit()
     sorted_products = sorted(products, key=lambda discount: discount[0], reverse=True)
-    print(sorted_products)
+    if sorted_products[0][2]:
+        specific_product(driver, sorted_products[0])
 
 
-def specific_product(driver):
-    title = driver.find_element(By.ID, "title")
+def specific_product(driver, product):
+    if product:
+        driver = webdriver.Firefox()
+        driver.get(product[2])
+        cookies = driver.find_element(By.CSS_SELECTOR, '#sp-cc-accept')
+        cookies.click()
+    title = get_title(driver)
     print(title)
-    base = driver.find_element(By.CLASS_NAME, "a-offscreen")
-    precio_entero = base.text.split('\n')
-    descuento = precio_entero[0]
-    descuento_sin_simbolo = descuento.replace('%', '')
-    precio_actual = precio_entero[1] + ',' + precio_entero[2]
-    print(descuento)
+
+    precio_actual = get_currentPrice(driver)
     print(precio_actual)
+    descuento = get_descuento(driver)
+    print(descuento)
+    precio_anterior = get_precio_anterior(driver)
+    print(precio_anterior)
+    descripcion = get_descripcion(driver)
+    print(descripcion)
+    foto_url = get_foto_url(driver)
+    print(foto_url)
 
     driver.quit()
 
 
 def amazn_scrap_selenium(url):
     url = 'https://www.amazon.es/deal/dd20fde3?'
+    url = 'https://www.amazon.es/Oral-B-Cepillos-El%C3%A9ctricos-Recargables-Recambio/dp/B0B4WSC72G'
 
-    browser = webdriver.Firefox()
-    browser.get(url)
-    cookies = browser.find_element(By.CSS_SELECTOR, '#sp-cc-accept')
+    driver = webdriver.Firefox()
+    driver.get(url)
+    cookies = driver.find_element(By.CSS_SELECTOR, '#sp-cc-accept')
     cookies.click()
 
     try:
         # Si es la pagina de un producto concreto, la llamada no fallara
-        base = browser.find_element(By.CLASS_NAME, 'a-section.a-spacing-none.aok-align-center')
-        specific_product(browser)
+        driver.find_element(By.CLASS_NAME, 'a-section.a-spacing-none.aok-align-center')
+        specific_product(driver, '')
 
 
     except Exception as e:
         # Si salta la excepcion es porque es una pagina que contiene varios productos
-        product_set(browser)
-
-
+        product_set(driver)
---
+++ utils_amzn_selenium.py
@@ -0,0 +1,64 @@
+from selenium.webdriver.common.by import By
+
+
+def get_title(driver):
+    try:
+        title = driver.find_element(By.ID, "title")
+    except Exception as e:
+        print(e)
+
+    return title.text
+
+
+def get_currentPrice(driver):
+    try:
+        current_price = driver.find_element(By.CLASS_NAME, "a-offscreen").text
+
+        if not current_price:
+            current_price = driver.find_element(By.CLASS_NAME,
+                                                "a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay")
+            precio_entero = str(current_price.text).split('\n')[0]
+            precio_decimal = str(current_price.text).split('\n')[1]
+            current_price = precio_entero + ',' + precio_decimal
+
+    except Exception as e:
+        print(e)
+
+    return current_price
+
+
+def get_precio_anterior(driver):
+    try:
+        precio_anterior = driver.find_element(By.CLASS_NAME, "a-price.a-text-price")
+    except Exception as e:
+        print(e)
+
+    return precio_anterior.text
+
+
+def get_descuento(driver):
+    try:
+        descuento = driver.find_element(By.CLASS_NAME,
+                                        "a-size-large.a-color-price.savingPriceOverride.aok-align-center.reinventPriceSavingsPercentageMargin.savingsPercentage")
+    except Exception as e:
+        print(e)
+
+    return descuento.text
+
+
+def get_descripcion(driver):
+    try:
+        descripcion = driver.find_element(By.CLASS_NAME, "a-unordered-list.a-vertical.a-spacing-mini")
+    except Exception as e:
+        print(e)
+
+    return descripcion.text
+
+
+def get_foto_url(driver):
+    try:
+        url = driver.find_element(By.ID, "landingImage").get_attribute('src')
+    except Exception as e:
+        print(e)
+
+    return url
